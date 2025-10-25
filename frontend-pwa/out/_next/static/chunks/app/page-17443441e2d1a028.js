3cd795b4a55ff74bb0a81475f80bb6d44f39b6e1
Ecoimbra1990 <ankecs@gmail.com>
2025-10-25 19:16:21 -0300

    feat: atualizar build do frontend com painel administrativo

--- frontend-pwa/out/_next/static/chunks/app/page-17443441e2d1a028.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4319:function(e,r,t){Promise.resolve().then(t.bind(t,3356))},3356:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return HomePage}});var n=t(7437),s=t(2265),o=t(4033);function HomePage(){let e=(0,o.useRouter)();return(0,s.useEffect)(()=>{e.push("/login")},[e]),(0,n.jsx)("div",{className:"min-h-screen flex items-center justify-center bg-gray-50",children:(0,n.jsxs)("div",{className:"text-center",children:[(0,n.jsx)("div",{className:"animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"}),(0,n.jsx)("p",{className:"text-gray-600",children:"Carregando SIRAOP..."})]})})}},622:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),s=Symbol.for("react.element"),o=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,c=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,o={},a=null,f=null;for(n in void 0!==t&&(a=""+t),void 0!==r.key&&(a=""+r.key),void 0!==r.ref&&(f=r.ref),r)u.call(r,n)&&!i.hasOwnProperty(n)&&(o[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===o[n]&&(o[n]=r[n]);return{$$typeof:s,type:e,key:a,ref:f,props:o,_owner:c.current}}r.Fragment=o,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)},4033:function(e,r,t){e.exports=t(94)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=4319)}),_N_E=e.O()}]);
